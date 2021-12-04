b5d9e829f33f1884895c1a7c87b684905b010e3a
Roman Voskoboynikov <64203160+Romindous@users.noreply.github.com>
2021-12-03 22:06:58 -0500

    добавляем аркаим задание

--- src/lbb/Romindous/Rom.java
+++ src/lbb/Romindous/Rom.java
@@ -3,39 +3,52 @@ package lbb.Romindous;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
-import org.bukkit.block.BlockFace;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import net.minecraft.core.BaseBlockPosition;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
 import ru.komiss77.Ostrov;
+import ru.ostrov77.lobby.LobbyFlag;
+import ru.ostrov77.lobby.LobbyPlayer;
+import ru.ostrov77.lobby.Main;
+
+/*цвета серверов
+PK - 7,b
+SN - 4,d
+MI - c,5
+SW - 3,2
+DA - a,6
+AK - 9,e
+PVP - c,6
+PVE - e,
+*/
 
 public class Rom {
-
-	private static HashMap<BaseBlockPosition, BaseBlockPosition> plts = new HashMap<BaseBlockPosition, BaseBlockPosition>();
-	private static HashMap<BaseBlockPosition, String> prts = new HashMap<BaseBlockPosition, String>();
-	private static HashMap<Player, BaseBlockPosition> tps = new HashMap<Player, BaseBlockPosition>();
-	private static final BlockFace[] nr = {BlockFace.DOWN, BlockFace.UP, BlockFace.SOUTH, BlockFace.NORTH, BlockFace.EAST, BlockFace.WEST};
-	private static Plugin instance;
-        
-        
-        
-	public static void onEnable(final Plugin plugin) {
-		instance = plugin;
+	
+	public static final HashMap<BaseBlockPosition, BaseBlockPosition> plts = new HashMap<BaseBlockPosition, BaseBlockPosition>();//плиты по типу точка начала : точка конца
+	public static final HashMap<BaseBlockPosition, String> prts = new HashMap<BaseBlockPosition, String>();//порталы по типу точка портала : 
+	public static final HashMap<Player, BaseBlockPosition> tps = new HashMap<Player, BaseBlockPosition>();//плиты по типу игрок : точка прибытия
+	public static final HashMap<String, HashSet<Material>> mts = new HashMap<String, HashSet<Material>>();//найденые блоки по типу ник : найденые материалы
+	public static Plugin instance;
+	
+	public static void onEnable(final Plugin plug) {
+		instance = plug;
 		Ostrov.log_ok("§2Lobby вкл.!");
-		Bukkit.getServer().getPluginManager().registerEvents(new RomListener(instance), instance);
+		Bukkit.getServer().getPluginManager().registerEvents(new RomListener(), plug);
 		
 		loadCfgs();
         
@@ -107,11 +120,39 @@ public class Rom {
 			}
         }
 	}
-
-        
-
 	
+	public static boolean chckAKTsk(final Player p) {
+		final LobbyPlayer lp = Main.getLobbyPlayer(p);
+		if (lp != null && !lp.hasFlag(LobbyFlag.Arcaim)) {
+			final HashSet<Material> ms = mts.get(p.getName());
+			if (ms == null) {
+				p.sendMessage("§9[§eНПС§9] §fЗдравствуй, будующий §eстроитель§f!");
+				Ostrov.sync(new Runnable() {
+					@Override
+					public void run() {
+						p.sendMessage("§fПодо мной находиться портал на §e§lАркаим§f,\n§fкреатив-сервер, ограниченный лишь твоей фантазией!");
+					}
+				}, 20);
+				Ostrov.sync(new Runnable() {
+					@Override
+					public void run() {
+						p.sendMessage("§fОднако, перед разблокировкой §dмгновенного перемещения §fтуда, тебе нужно изучить блоки в этом лобби!\n§6[§fНайди §e50 §fразных §eблоков §fв этом лобби§6]");
+						//bossbar???
+						mts.put(p.getName(), new HashSet<Material>());
+					}
+				}, 80);
+			} else if (ms.size() > 50) {
+				p.sendMessage("§9[§eНПС§9] §fМолодец, тебе удалось найти различные §eблоки §fв этом лобби! Теперь ты можешь §dмгновенно §fперемещатся на §e§lАркаим§f!");
+				lp.setFlag(LobbyFlag.Arcaim, true);
+				mts.remove(p.getName());
+			} else {
+				p.sendMessage("§9[§eНПС§9] §fОсталось найти всего §e" + (50 - ms.size()) + " §fблок(ов)!");
+			}
+		}
+		return false;
+	}
 	
-
-
+	/*public static String OCPrfx() {
+		return "§6[§eОстров§6] ";
+	}*/
 }

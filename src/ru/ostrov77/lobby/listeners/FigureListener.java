4ca91e6ef68aae690971d2ed64ce4edf3a4a5cd8
BuildTools <unconfigured@null.spigotmc.org>
2021-12-26 23:14:32 +0300

    Merge origin/master
    Conflicts:
    	src/do
    	src/ru/ostrov77/lobby/Main.java
    	src/ru/ostrov77/lobby/area/AreaViewMenu.java
    	src/ru/ostrov77/lobby/area/CuboidInfo.java
    	src/ru/ostrov77/lobby/listeners/CosmeticListener.java
    	src/ru/ostrov77/lobby/listeners/ListenerWorld.java
    	src/ru/ostrov77/lobby/quest/QuestManager.java

--- src/ru/ostrov77/lobby/listeners/FigureListener.java
+++ src/ru/ostrov77/lobby/listeners/FigureListener.java
@@ -15,6 +15,7 @@ import ru.komiss77.objects.FigureAnswer;
 import ru.ostrov77.lobby.LobbyFlag;
 import ru.ostrov77.lobby.LobbyPlayer;
 import ru.ostrov77.lobby.Main;
+import ru.ostrov77.lobby.area.LCuboid;
 import ru.ostrov77.lobby.quest.Quest;
 import ru.ostrov77.lobby.quest.QuestManager;
 
@@ -30,8 +31,35 @@ public class FigureListener implements Listener {
         final Player p = e.getPlayer();
 //p.sendMessage("§8log: tag="+e.getFigure().getTag()+" left?"+e.isLeftClick());
         final LobbyPlayer lp = Main.getLobbyPlayer(p);
-        if (e.getFigure().getTag().equals("info")) {
-        	final FigureAnswer fa = new FigureAnswer();
+        
+        if (e.getFigure().getTag().equals("ship")) {
+            if (e.isLeftClick()) {
+                e.setAnswer((new FigureAnswer()).add("§cНе бейте, а гладьте))", (player) -> {
+                    LCuboid lc = lp.getCuboid();
+                    p.sendMessage(lc == null ? "ты не в кубоиде" : "ты в кубоиде:" + lc.displayName);
+                }));
+            } else {
+                FigureAnswer answer = new FigureAnswer()
+                        //.add("§eПривет!", (player) -> {player.sendMessage("eПривет");} )
+                        //.add("§aТы тут новенький?", (player) -> {player.sendMessage("новенький");} )
+                        //.add("§bНа корабле есть §lособенная лампа §r§b,потри её.", (player) -> {player.sendMessage("особенная лампа");} )
+                       // .add(Material.SOUL_LANTERN, (player) -> {player.sendMessage("pickup SOUL_LANTERN");} )
+                        //.add("§6Ну или прыгай за борт.", (player) -> {player.sendMessage("прыгай");} )
+                        .add("§eПривет!" )
+                        .add("§aТы тут новенький?")
+                        .add("§bНа корабле есть §lособенная лампа §r§b,потри её.")
+                        .add(Material.SOUL_LANTERN)
+                        .add("§6Ну или прыгай за борт.")
+                        .time(15).vibration().sound(Sound.ENTITY_VILLAGER_TRADE)
+                        .beforeEyes()
+                        ;
+                e.setAnswer(answer);
+                QuestManager.tryCompleteQuest(p, lp, Quest.SpeakWithNPC);
+            }
+            
+        } else if (e.getFigure().getTag().equals("info")) {
+            
+            final FigureAnswer fa = new FigureAnswer();
             switch (e.getFigure().getEntityType()) {
     		case ENDERMAN:
     			fa.set(Arrays.asList("Добро пожаловать на §9Аркаим§f,", "На этом режиме вы сможете", "проявить свою полную фантазию", "благодаря халявному §9креативу§f!", "§eУдачного строительства!"))
@@ -82,6 +110,7 @@ public class FigureListener implements Listener {
     			break;
     		}
             e.setAnswer(fa.vibration().beforeEyes());
+            
         } else if (e.getFigure().getTag().equals("mid") && e.getFigure().getEntityType() == EntityType.PIGLIN) {
 			switch (e.getFigure().getName().charAt(4)) {
 			case 'K':

d190aa7c4caedea6f7556b9df102562118fb8fed
BuildTools <unconfigured@null.spigotmc.org>
2025-01-30 20:40:10 +0300

    обнова описания серверов

--- src/ru/ostrov77/lobby/listeners/FigureListener.java
+++ src/ru/ostrov77/lobby/listeners/FigureListener.java
@@ -38,9 +38,9 @@ public class FigureListener implements Listener {
 
             switch (e.getFigure().getEntityType()) {
 
-                case ENDERMAN:
+                case ENDERMAN -> {
                     if (e.isRightClick()) {
-                        fa.set(Arrays.asList(tp + "Добро пожаловать на §9Аркаим§f. На этом режиме ты сможещь проявить свою полную §9фантазию§f благодаря халявному §9креативу§f!"))
+                        fa.set(Arrays.asList(tp + " §9Аркаим - §бТвой собственный мир, §бв котором ты полный хозяин! §бЛюбой игровой режим. §eИграй один или с друзьями!" ))
                                 .sound(Sound.ENTITY_ENDERMAN_AMBIENT);
                         QuestManager.addProgress(p, lp, Quests.agent, "AR");
                     } else {
@@ -48,11 +48,11 @@ public class FigureListener implements Listener {
                         lp.transport(p, new XYZ(AreaManager.getCuboid(CuboidInfo.ARCAIM).getSpawnLocation(p.getWorld())), false);
                         return;
                     }
-                    break;
+                }
 
-                case PIGLIN_BRUTE:
+                case PIGLIN_BRUTE -> {
                     if (e.isRightClick()) {
-                        fa.set(Arrays.asList(tp + "Здравствуй, товарищ, и добро пожаловать в §cСове§f... точнее на §cМидгард§f - современный §cРП-режим§f, где тебе и твоим друзьям предстоит построить §cимперию§f с нуля! §к(В Разработке)"))
+                        fa.set(Arrays.asList(tp + "Здравствуй, товарищ, и добро пожаловать в §cСове§f... точнее на §cМидгард§f - современный §cКлановый режим§f, где тебе и твоим друзьям предстоит построить §cимперию§f с нуля!"))
                                 .sound(Sound.ENTITY_PIGLIN_BRUTE_AMBIENT);
                         QuestManager.addProgress(p, lp, Quests.agent, "MI");
                     } else {
@@ -60,9 +60,9 @@ public class FigureListener implements Listener {
                         lp.transport(p, new XYZ(AreaManager.getCuboid(CuboidInfo.MIDGARD).getSpawnLocation(p.getWorld())), false);
                         return;
                     }
-                    break;
+                }
 
-                case ZOMBIE_VILLAGER:
+                case ZOMBIE_VILLAGER -> {
                     if (e.isRightClick()) {
                         fa.set(Arrays.asList(tp + "Приветствую тебя, игрок, на режиме §aДаарии§f - ванильном выживании, с поддержкой всех последних обновлений §aМайнкрафта§f!"))
                                 .sound(Sound.ENTITY_ZOMBIE_VILLAGER_AMBIENT);
@@ -71,9 +71,9 @@ public class FigureListener implements Listener {
                         DisplayManager.rmvDis(p);
                         lp.transport(p, new XYZ(AreaManager.getCuboid(CuboidInfo.DAARIA).getSpawnLocation(p.getWorld())), false);
                     }
-                    break;
+                }
 
-                case VILLAGER:
+                case VILLAGER -> {
                     if (lp.hasFlag(LobbyFlag.GinTravelDone)) {
                         if (e.isRightClick()) {
                             fa.set(Arrays.asList(tp + "§fРад видеть тебя снова!")).sound(Sound.ENTITY_VILLAGER_AMBIENT);
@@ -88,9 +88,9 @@ public class FigureListener implements Listener {
                                 .sound(Sound.ENTITY_VILLAGER_AMBIENT);
                         QuestManager.complete(p, lp, Quests.locman);
                     }
-                    break;
+                }
 
-                case ZOMBIE:
+                case ZOMBIE -> {
                     if (e.isRightClick()) {
                         fa.set(Arrays.asList(tp + "Здесь режим §3Ван-Блок§f, на котором тебе предстоит построить §3островок§f буквально начиная только с §31 блоком§f! §3Скай-Блок§f, если что, дальше §3-->"))
                                 .sound(Sound.ENTITY_ZOMBIE_AMBIENT);
@@ -100,9 +100,9 @@ public class FigureListener implements Listener {
                         lp.transport(p, new XYZ(AreaManager.getCuboid(CuboidInfo.SKYWORLD).getSpawnLocation(p.getWorld())), false);
                         return;
                     }
-                    break;
+                }
 
-                case WITCH:
+                case WITCH -> {
                     if (e.isRightClick()) {
                         fa.set(Arrays.asList(tp + "Приветствую, странник, приглашаю тебя на §3Скай-Блок§f - режим, заключенный в §3развити§f и §3прокачке§f острова, так сказать, из грязи в князи!"))
                                 .sound(Sound.ENTITY_WITCH_AMBIENT);
@@ -112,9 +112,9 @@ public class FigureListener implements Listener {
                         lp.transport(p, new XYZ(AreaManager.getCuboid(CuboidInfo.SKYWORLD).getSpawnLocation(p.getWorld())).add(0, -1, -20), false);
                         return;
                     }
-                    break;
+                }
 
-                case WANDERING_TRADER:
+                case WANDERING_TRADER -> {
                     if (e.isRightClick()) {
                         fa.set(Arrays.asList(tp + "Привет, друг! Здесь, ты можешь §eразвлечься§f в ассортименте разных мини-игр, таких как §eКонтра§f, §eБилд Баттл§f, §eПрятки§f, и т.д., как и сам, так и со своими друзьями!"))
                                 .sound(Sound.ENTITY_WANDERING_TRADER_AMBIENT);
@@ -124,9 +124,9 @@ public class FigureListener implements Listener {
                         lp.transport(p, new XYZ(AreaManager.getCuboid(CuboidInfo.NOPVP).getSpawnLocation(p.getWorld())), false);
                         return;
                     }
-                    break;
+                }
 
-                case SNOW_GOLEM:
+                case SNOW_GOLEM -> {
                     if (e.isRightClick()) {
                         fa.set(Arrays.asList(tp + "Бурлит §bадреналин§f в крови? Тогда ты по адресу, ведь на §bПаркурах§f, у тебя есть выбор среди §b60+ карт§f, все из них с интересными темами и прыжками!"))
                                 .sound(Sound.BLOCK_SNOW_BREAK);
@@ -136,9 +136,9 @@ public class FigureListener implements Listener {
                         lp.transport(p, new XYZ(AreaManager.getCuboid(CuboidInfo.PARKUR).getSpawnLocation(p.getWorld())), false);
                         return;
                     }
-                    break;
+                }
 
-                case WITHER_SKELETON:
+                case WITHER_SKELETON -> {
                     if (e.isRightClick()) {
                         fa.set(Arrays.asList(tp + "Сможешь ли ты показать свой скилл на поле боя? Выбери §6PVP§f мини-игру на свой вкус (§6Кит-ПВП§f, §6Скай Варс§f, §6Зомби§f, и т.д.) и отжигай местную фауну с друзьями!"))
                                 .sound(Sound.ENTITY_WITHER_SKELETON_AMBIENT);
@@ -148,9 +148,9 @@ public class FigureListener implements Listener {
                         lp.transport(p, new XYZ(AreaManager.getCuboid(CuboidInfo.PVP).getSpawnLocation(p.getWorld())), false);
                         return;
                     }
-                    break;
+                }
 
-                case ZOMBIFIED_PIGLIN:
+                case ZOMBIFIED_PIGLIN -> {
                     if (e.isRightClick()) {
                         fa.set(Arrays.asList(tp + "Здравствуй, §4скиталец§f. Думаешь что обычные мобы тебе больше не помеха? Тогда залетай на §4Хардкор-РПГ §fмиры Седны, выбирай класс, и покажи насколько ты годен в гуще §4кровавого §fзамеса!"))
                                 .sound(Sound.ENTITY_ZOMBIFIED_PIGLIN_AMBIENT);
@@ -159,17 +159,17 @@ public class FigureListener implements Listener {
                         DisplayManager.rmvDis(p);
                         lp.transport(p, new XYZ(AreaManager.getCuboid(CuboidInfo.SEDNA).getSpawnLocation(p.getWorld())), false);
 //					p.teleport(AreaManager.getCuboid(CuboidInfo.SEDNA).getSpawnLocation(p.getWorld())), false;
-                        return;
+return;
                     }
-                    break;
+                }
 
-                case CREEPER:
+                case CREEPER -> {
                     fa.set(Arrays.asList("Приветствую, §eпутник§f, и добро пожаловать на §3Архипелаг§f - лобби, где на каждом острове ты найдешь интересные и уникальные §6Большие Режимы§f, или §eМини-Игры§f для тебя и твоих друзей! §eИсследуй весь остров!"))
                             .sound(Sound.ENTITY_CREEPER_HURT);
                     QuestManager.addProgress(p, lp, Quests.agent, "LB");
-                    break;
-                default:
-                    break;
+                }
+                default -> {
+                }
 
             }
 
